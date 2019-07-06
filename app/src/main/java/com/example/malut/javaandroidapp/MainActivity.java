4a597deb9d41fe139861bcfd94912da35bb22b1c
Katerina <malutina.catya14@gmail.com>
2019-07-06 19:53:25 +0300

    Colorful 'Hello World'.

--- app/src/main/java/com/example/malut/javaandroidapp/MainActivity.java
+++ app/src/main/java/com/example/malut/javaandroidapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.malut.javaandroidapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloWorldText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloWorldText = findViewById(R.id.hello_world_text);
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                helloWorldText.setTextColor(Color.rgb((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255)));
+            }
+        }, 0, 1000);
     }
 }
