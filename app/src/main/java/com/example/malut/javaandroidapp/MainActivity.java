cb4fe645f51daec1c2949e66cafd66585f49dc11
Katerina <malutina.catya14@gmail.com>
2019-07-11 19:32:56 +0300

    Screen with input fields.

--- app/src/main/java/com/example/malut/javaandroidapp/MainActivity.java
+++ app/src/main/java/com/example/malut/javaandroidapp/MainActivity.java
@@ -10,20 +10,10 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView helloWorldText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        helloWorldText = findViewById(R.id.hello_world_text);
-        Timer timer = new Timer();
-
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                helloWorldText.setTextColor(Color.rgb((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255)));
-            }
-        }, 0, 1000);
     }
 }
